2cac5e6818871fd2affecdbcec2dbcd50f5b0ceb
Cesar Bianchi <cesar_bianchi@hotmail.com>
2021-11-23 22:56:25 -0300

    Not Ready!

---
+++ src/main/java/br/com/lrsbackup/LRSUploadEngine/LrsBackupApplication.java
@@ -0,0 +1,13 @@
+package br.com.lrsbackup.LRSUploadEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrsBackupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LrsBackupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lrsbackup/LRSUploadEngine/LrsBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lrsbackup.LRSUploadEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrsBackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
